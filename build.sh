995f0e2abe81eece723153aa43586ced2bc20357
brian guzman <neikiam500@gmail.com>
2025-11-26 16:29:23 -0300

    Fix: Add makemigrations to build.sh to create database tables

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ find . -type d -name __pycache__ -exec rm -r {} + 2>/dev/null || true
 find . -type f -name "*.pyc" -delete 2>/dev/null || true
 pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+python manage.py collectstatic --no-input
