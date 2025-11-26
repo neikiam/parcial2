cb9d9cdfa1513788521f90a250c2d1eb16debff5
brian guzman <neikiam500@gmail.com>
2025-11-26 15:44:49 -0300

    Add Render domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-produc
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,parcial2-deploy.onrender.com', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
